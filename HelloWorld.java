10fd66c37e54d24b998fe2454f1b80c1452dc268
soupp1 <nicholas.doane@gmail.com>
2021-02-03 09:58:29 -0500

    Added goodbye message to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hear ye: important announcement!");
         System.out.println("Hello World!");
-
+        System.out.println("That is all, I take my leave!");
     }
 
 }
